e6ca25e756d6c3ec1ef7c1b8a3639bb8ce53e9b4
Benjamin Semah <benjaminsemah@gmail.com>
2022-02-17 19:16:52 +0000

    Refactor components to functional components

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@
 import React from 'react';
 import Calculator from './components/Calculator';
 
-class App extends React.Component {
-  render() {
-    return (
-      <>
-        <Calculator />
-      </>
-    );
-  }
-}
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <>
+//         <Calculator />
+//       </>
+//     );
+//   }
+// }
+
+const App = () => (
+  <>
+    <Calculator />
+  </>
+);
 
 export default App;
